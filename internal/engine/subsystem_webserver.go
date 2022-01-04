68c37aff76754e407463a1bf66bb455a90f244b1
sudoshu <sudoshu@protonmail.com>
2022-01-03 23:52:39 -0500

    Removed old commented code. Updated function call to start web server.

--- internal/engine/subsystem_webserver.go
+++ internal/engine/subsystem_webserver.go
@@ -20,7 +20,7 @@ func (s *WebserverSubsystem) init(bot *Engine, name string) error {
 		return err
 	}
 
-	s.server, err = webserver.CreateWebserver(bot, bot.Config.Webserver, false)
+	s.server, err = webserver.CreateWebserver(bot, bot.Config.Webserver)
 	if err != nil {
 		log.Errorf(log.Global, "Error creating webserver: %v", err)
 	}
@@ -38,10 +38,10 @@ func (s *WebserverSubsystem) start(wg *sync.WaitGroup) (err error) {
 
 	s.server.SetupRoutes(s.bot.IsDevelopment)
 
-	//wg.Add(1)
-	//s.wg.Add(1)
+	wg.Add(1)
+	s.wg.Add(1)
 	//go s.run()
-	go s.server.StartWebServer(s.bot.IsDevelopment, s.shutdown)
+	go s.server.StartWebServer(true, s.bot.IsDevelopment, s.shutdown, s.bot.Config.ConfigDir)
 
 	s.started = true
 	log.Debugln(log.Webserver, s.name+MsgSubsystemStarted)
@@ -53,36 +53,9 @@ func (s *WebserverSubsystem) stop() error {
 	if err := s.Subsystem.stop(); err != nil {
 		return err
 	}
+	s.started = false
 	close(s.shutdown)
 	s.wg.Wait()
-	s.started = false
 	log.Debugln(log.Webserver, s.name+MsgSubsystemShutdown)
 	return nil
-}
-
-// run this is the main loop for the subsystem
-func (s *WebserverSubsystem) run() {
-
-	//s.server.StartWebServer()
-	// Start the Node client app (only for version "development")
-	/*if s.bot.Config.IsDevelopment {
-		go webserver.StartNodeDevelopmentServer()
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
-	defer func() {
-		cancel()
-		qstlog.Debugln(qstlog.Webserver, "Webserver subsystem shutdown.")
-	}()
-
-	_ = s.server.HttpServer.ListenAndServe()
-	qstlog.Infof(qstlog.Webserver, "Web server started, listening on http://%v\n", ":8080")
-
-
-
-	// Code below will only run if we get a message on the shutdown channel
-	// Remember, we are inside a goroutine right now
-	<-s.shutdown
-	_ = s.server.HttpServer.Shutdown(ctx)*/
 }
