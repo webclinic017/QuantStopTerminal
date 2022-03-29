11d891e4f260324c4b5d23210a8f2caece76e166
sudoshu <sudoshu@protonmail.com>
2022-03-29 19:27:32 -0400

    updates

--- internal/grpcserver/auth/jwt.go
+++ internal/grpcserver/auth/jwt.go
@@ -2,26 +2,29 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/quantstop/quantstopterminal/internal/log"
 	"google.golang.org/grpc/metadata"
 	"time"
 )
 
-func CreateToken(userName string) (tokenString string) {
+func CreateToken(userName string) (tokenString string, err error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"iss":      "lora-app-server",
-		"aud":      "lora-app-server",
+		"iss":      "quantstopterminal",
+		"aud":      "quantstopterminal",
 		"nbf":      time.Now().Unix(),
 		"exp":      time.Now().Add(time.Hour).Unix(),
 		"sub":      "user",
 		"username": userName,
 	})
-	tokenString, err := token.SignedString([]byte("verysecret"))
+	tokenString, err = token.SignedString([]byte("ajaeyuiuop5n32o9s0g"))
 	if err != nil {
-		panic(err)
+		log.Errorf(log.GRPClog, "jwt error: %v", err)
+		return "", err
 	}
-	return tokenString
+	return tokenString, nil
 }
 
 // Token  Custom authentication
@@ -39,7 +42,7 @@ func (c Token) RequireTransportSecurity() bool {
 	return false
 }
 
-// Claims defines the struct containing the token claims.
+// Claims defines the struct containing the token claims
 type Claims struct {
 	jwt.StandardClaims
 
@@ -47,41 +50,60 @@ type Claims struct {
 	Username string `json:"username"`
 }
 
-// Step1.  from  context  Of  metadata  in , Take out  token
-
+// getTokenFromContext reads incoming context and extracts auth token from metadata
 func getTokenFromContext(ctx context.Context) (string, error) {
+
+	// try and get the metadata from context
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
 		return "", fmt.Errorf("ErrNoMetadataInContext")
 	}
-	// md  The type is  type MD map[string][]string
-	token, ok := md["authorization"]
-	if !ok || len(token) == 0 {
+
+	// metadata type is map[string][]string of all request headers
+	// token stored in authorization header
+	// try and get token from header
+	authHeader, ok := md["authorization"]
+	if !ok || len(authHeader) == 0 {
 		return "", fmt.Errorf("ErrNoAuthorizationInMetadata")
 	}
-	//  therefore ,token  Is an array of strings , We only use  token[0]
-	return token[0], nil
+
+	// success, authHeader is an array of strings, token stored in first slice
+	return authHeader[0], nil
 }
 
-func CheckAuth(ctx context.Context) (username string) {
+// CheckAuth verifies jwt
+func CheckAuth(ctx context.Context) (username string, err error) {
+
+	// try and get the token from incoming header
 	tokenStr, err := getTokenFromContext(ctx)
 	if err != nil {
-		panic("get token from context error")
+		return "", err
 	}
+
+	// try and parse token into clientClaims
 	var clientClaims Claims
 	token, err := jwt.ParseWithClaims(tokenStr, &clientClaims, func(token *jwt.Token) (interface{}, error) {
+
+		// check if algo matches
 		if token.Header["alg"] != "HS256" {
-			panic("ErrInvalidAlgorithm")
+			//panic("ErrInvalidAlgorithm")
+			return "", errors.New("invalid token algorithm")
 		}
-		return []byte("verysecret"), nil
+
+		// return secret, no error
+		return []byte("ajaeyuiuop5n32o9s0g"), nil
 	})
+
+	// return any error from token parsing
 	if err != nil {
-		panic("jwt parse error")
+		return "", err
 	}
 
+	// return error if token isn't valid
 	if !token.Valid {
-		panic("ErrInvalidToken")
+		return "", err
 	}
 
-	return clientClaims.Username
+	// success return username
+	return clientClaims.Username, nil
 }
