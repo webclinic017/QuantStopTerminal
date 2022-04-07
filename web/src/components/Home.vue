520ad10b4a4d23b36ddd1f501e9f5cc87e6ecf2e
sudoshu <sudoshu@protonmail.com>
2022-04-06 21:46:27 -0400

    playing with theme, work in progress ...

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container py-4">
-    <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="p-5 mb-4 rounded-3 card">
       <div class="container-fluid py-5">
         <h3>{{ content }}</h3>
       </div>
