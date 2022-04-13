2c0e6aaba2c141c570a18994f05bfcc1a25c8c8e
sudoshu <sudoshu@protonmail.com>
2022-04-12 21:19:54 -0400

    moved header/navbar to own component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,135 +1,50 @@
 <template>
-  <div id="app">
 
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <div class="container-fluid">
-        <router-link to="/" class="navbar-brand nav-link">QuantstopTerminal</router-link>
+  <!--  Header / Navbar  -->
+  <Header />
 
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <router-link to="/home" class="nav-link">
-              <font-awesome-icon icon="home" /> Home
-            </router-link>
-          </li>
-          <li v-if="showAdminBoard" class="nav-item">
-            <router-link to="/admin" class="nav-link">Admin Board</router-link>
-          </li>
-          <li v-if="showModeratorBoard" class="nav-item">
-            <router-link to="/mod" class="nav-link">Moderator Board</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link v-if="getUserProfile.id !== 0" to="/user" class="nav-link">User</router-link>
-          </li>
-        </ul>
+  <!--  Main  -->
+  <main class="d-flex align-content-stretch">
+    <router-view />
+  </main>
 
-        <div class="nav-item">
-          <theme-button />
-        </div>
-
-        <div v-if="getUserProfile.id === 0" class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/register" class="nav-link">
-              <font-awesome-icon icon="user-plus" /> Sign Up
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/login" class="nav-link">
-              <font-awesome-icon icon="sign-in-alt" /> Login
-            </router-link>
-          </li>
-        </div>
-
-        <div v-if="getUserProfile.id !== 0" class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/profile" class="nav-link">
-              <font-awesome-icon icon="user" />
-              {{ getUserProfile.username }}
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" @click.prevent="logOut" href="/logout">
-              <font-awesome-icon icon="sign-out-alt" /> LogOut
-            </a>
-          </li>
-        </div>
-
-      </div>
-    </nav>
-
-    <div class="container">
-      <router-view />
-    </div>
-  </div>
+  <!--  Footer  -->
+  <Footer />
 </template>
 
 <script>
-import { mapGetters, mapActions, mapMutations } from "vuex";
-import ThemeButton from "./components/ThemeButton.vue"
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer";
 export default {
   name: 'App',
   components: {
-    ThemeButton
+    Header,
+    Footer
   },
-  computed: {
-    ...mapGetters("auth", {
-      getUserProfile: "getUserProfile",
-      getLogout: "getLogout",
-    }),
-    showAdminBoard() {
-      if (this.getUserProfile && this.getUserProfile['roles']) {
-        return this.getUserProfile['roles'].includes('admin');
-      }
 
-      return false;
-    },
-    showModeratorBoard() {
-      if (this.getUserProfile && this.getUserProfile['roles']) {
-        return this.getUserProfile['roles'].includes('moderator');
-      }
-
-      return false;
-    }
+  mounted () {
+    document.body.classList.add('d-flex', 'flex-column', 'h-100')
+    document.documentElement.classList.add('h-100')
   },
-  methods: {
-    ...mapActions("auth", {
-      userLogout: "userLogout",
-    }),
-    ...mapMutations("auth", {
-      setLogout: "setLogout",
-      setUserProfile: "setUserProfile",
-    }),
-    async logOut() {
-      await this.userLogout();
-      if (this.getLogout) {
-        const resetUser = {
-          id: 0,
-          username: "",
-          roles: null,
-        };
-        this.setUserProfile(resetUser);
-        this.setLogout(false);
-        this.$router.push("/");
-      }
-    }
-  }
 }
 </script>
 
 <style>
-#app {
-  background-color: var(--background-color-primary);
-}
-html,
 body {
-  background-color: var(--background-color-primary);
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-color: var(--background-color-primary) !important;
+}
 
+main {
+  background-color: var(--background-color-primary);
+  margin-top: 55px;
+  margin-bottom: auto;
+}
+footer {
+  background-color: var(--background-color-primary);
 }
 /* Define styles for the default root window element */
 :root {
-  --background-color-primary: #ebebeb;
+  --background-color-primary: #fff;
   --background-color-secondary: #fafafa;
   --accent-color: #cacaca;
   --text-primary-color: #222;
