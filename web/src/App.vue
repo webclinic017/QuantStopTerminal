62414ade79127685cf856b2e87b1ee9a0bfdeb3c
sudoshu <sudoshu@protonmail.com>
2022-04-13 23:25:43 -0400

    theme button updates

--- web/src/App.vue
+++ web/src/App.vue
@@ -42,34 +42,32 @@ main {
 footer {
   background-color: var(--background-color-primary);
 }
+
 /* Define styles for the default root window element */
 :root {
-  --background-color-primary: #fff;
+  --background-color-primary: #ffffff;
   --background-color-secondary: #fafafa;
   --accent-color: #cacaca;
   --text-primary-color: #222;
   --element-size: 4rem;
+  --theme-switch-background-color: #f1f1f1; /* Theme switch background color */
+  --theme-switch-border-color: rgb(60 60 60 / 29%);
+  --theme-switch-border-color-hover: rgba(59, 59, 59, 0.29);
 }
 
 /* Define styles for the root window with dark - mode preference */
 :root.dark-theme {
-  --background-color-primary: #1e1e1e;
+  --background-color-primary: #1a1a1a;
   --background-color-secondary: #2d2d30;
   --accent-color: #3f3f3f;
-  --text-primary-color: #ddd;
+  --text-primary-color: rgb(255 255 255 / 87%);
+  --theme-switch-background-color: #2f2f2f; /* Theme switch background color */
+  --theme-switch-border-color: rgb(84 84 84 / 65%);
+  --theme-switch-border-color-hover: rgba(196, 193, 193, 0.29);
 }
 
-p {
-  color: var(--text-primary-color);
-}
-
-.container {
-  background-color: var(--background-color-primary);
-  /*height: 100vh;
-  width: 100vw;
-  display: flex;
-  align-items: center;
-  justify-content: center;*/
+a, p, .nav-link {
+  color: var(--text-primary-color) !important;
 }
 
 </style>
--- web/src/components/ThemeButton.vue
+++ web/src/components/ThemeButton.vue
@@ -1,29 +1,32 @@
 <template>
   <div>
     <input
-        @change="toggleTheme"
-        @click="isActive = !isActive"
-        id="checkbox"
-        type="checkbox"
-        class="switch-checkbox"
+      @change="toggleTheme"
+      @click="isActive = !isActive"
+      id="checkbox"
+      type="checkbox"
+      class="switch-checkbox"
     />
     <label for="checkbox" class="switch-label">
-
-      <div class="switch-toggle" :class="{ 'switch-toggle-checked': userTheme === 'dark-theme' }">
-
-        <!-- sun-->
-<!--          <svg :class="{ hidden: isActive }" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun switch-toggle-icon" viewBox="0 0 16 16">
-            <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-          </svg>-->
-
-        <!-- moon-->
-<!--        <svg :class="{ hidden: !isActive }" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars switch-toggle-icon" viewBox="0 0 16 16" style="color: white">
-          <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
-          <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
-        </svg>-->
-
-      </div>
-
+      <span class="switch-toggle" :class="{ 'switch-toggle-checked': userTheme === 'dark-theme' }">
+        <svg class="sun-and-moon" aria-hidden="true" width="22.4" height="22.4" viewBox="0 0 24 24">
+          <circle class="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" stroke="currentColor" :style="{'fill': isActive ? 'currentColor': 'none'}" />
+          <g class="sun-beams" stroke="currentColor">
+            <line x1="12" y1="2" x2="12" y2="4" />
+            <line x1="12" y1="20" x2="12" y2="22" />
+            <line x1="5.22" y1="5.22" x2="6.64" y2="6.64" />
+            <line x1="17.36" y1="17.36" x2="18.78" y2="18.78" />
+            <line x1="2" y1="12" x2="4" y2="12" />
+            <line x1="20" y1="12" x2="22" y2="12" />
+            <line x1="5.22" y1="18.78" x2="6.64" y2="17.36" />
+            <line x1="17.36" y1="6.64" x2="18.78" y2="5.22" />
+          </g>
+          <mask class="moon" id="moon-mask">
+            <rect x="0" y="0" width="100%" height="100%" fill="white" />
+            <circle cx="22.4" cy="7" r="5" fill="black" />
+          </mask>
+        </svg>
+      </span>
     </label>
   </div>
 </template>
@@ -48,6 +51,7 @@ export default {
       const activeTheme = localStorage.getItem("user-theme");
       if (activeTheme === "light-theme") {
         this.setTheme("dark-theme");
+
       } else {
         this.setTheme("light-theme");
       }
@@ -79,48 +83,78 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+/* helpful resources:
+  https://web.dev/building-a-theme-switch-component/
+*/
+@import "https://unpkg.com/open-props/easings.min.css";
+
 .switch-checkbox {
-  display: none;
-}
-.hidden {
   display: none;
 }
 .switch-label {
   align-items: center;
-  background: var(--text-primary-color);
-  border: calc(var(--element-size) * 0.025) solid var(--accent-color);
+  background: var(--theme-switch-background-color);
+  border: calc(var(--element-size) * 0.025) solid var(--theme-switch-border-color);
   border-radius: var(--element-size);
   cursor: pointer;
   display: flex;
   font-size: calc(var(--element-size) * 0.3);
   height: calc(var(--element-size) * 0.4);
   position: relative;
-  /*padding: calc(var(--element-size) * 0.1);*/
   transition: background 0.5s ease;
   justify-content: space-between;
   width: calc(var(--element-size) * 0.8);
   z-index: 1;
 }
 
+.switch-label:hover {
+  border: calc(var(--element-size) * 0.025) solid var(--theme-switch-border-color-hover);
+}
 .switch-toggle {
   position: absolute;
   background-color: var(--background-color-primary);
   border-radius: 50%;
-  /*top: calc(var(--element-size) * 0.07);
-  left: calc(var(--element-size) * 0.07);*/
-  height: calc(var(--element-size) * 0.4);
-  width: calc(var(--element-size) * 0.4);
+  height: calc(var(--element-size) * 0.35);
+  width: calc(var(--element-size) * 0.35);
   transform: translateX(0);
   transition: transform 0.25s ease, background-color 0.5s ease;
   z-index: 2;
 }
-
-.switch-toggle-icon {
-  margin-bottom: 8px;
-  margin-left: 4px;
-  z-index: 3;
-}
 .switch-toggle-checked {
   transform: translateX(calc(var(--element-size) * 0.4)) !important;
+
 }
+
+/* styles for sun/moon icon */
+.switch-toggle-checked .sun {
+  transition-timing-function: var(--ease-3);
+  transition-duration: .25s;
+}
+.switch-toggle-checked .sun-beams {
+  opacity: 0;
+  transform: rotateZ(-25deg);
+  transition-duration: .15s;
+}
+.switch-toggle-checked .moon circle {
+  transform: translateX(-7px);
+  transition: transform .25s var(--ease-out-5);
+  transition-delay: .25s;
+  transition-duration: .5s;
+}
+.sun-and-moon {
+  display:flex;
+  align-items:center;
+  justify-content:center;
+}
+.sun {
+  transition: transform .5s var(--ease-elastic-3);
+}
+.sun-beams {
+  transition:
+    transform .5s var(--ease-elastic-4),
+    opacity .5s var(--ease-3)
+  ;
+}
+
 </style>
