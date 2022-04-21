cb6be14d5e1475f946daac38f0696bfd89bc3877
sudoshu <sudoshu@protonmail.com>
2022-04-20 20:49:37 -0400

    deprecated genassets, using embed instead

---
+++ web/embed_dev.go
@@ -0,0 +1,15 @@
+//go:build !prod
+// +build !prod
+
+package web
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+func GetFileSystem() fs.FS {
+	fmt.Println("Filesystem served by os package")
+	return os.DirFS("web/dist")
+}
