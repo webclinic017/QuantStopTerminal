767c70ca3da511ba3aba5744295b666ed9972e71
sudoshu <sudoshu@protonmail.com>
2022-04-25 12:12:33 -0400

    added charting library

---
+++ web/src/components/klinechart/examples/Layout.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="k-line-chart-container w-100">
+    <h3 class="k-line-chart-title">{{title}}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  props: {
+    title: String
+  },
+}
+</script>
