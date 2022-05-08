f846307635ac0392561463ff6ddb91887e8d236e
sudoshu <sudoshu@protonmail.com>
2022-05-08 19:16:24 -0400

    added global event bus

--- web/src/main.js
+++ web/src/main.js
@@ -5,8 +5,10 @@ import store from './store/index'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from './plugins/font-awesome'
+import $bus from "./websocket/events.js"
 
 const app = createApp(App)
+app.config.globalProperties.$bus = $bus;
 app.use(appRouter.routeConfig);
 app.use(store);
 app.component("font-awesome-icon", FontAwesomeIcon)
